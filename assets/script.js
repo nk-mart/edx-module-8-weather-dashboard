79679add90e02294d65329e19d9366231fb4e194
nk-mart <113395569+nk-mart@users.noreply.github.com>
2023-02-01 01:29:40 +0000

    Update js
    Added fetch code block- to hit api endpoints

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,23 @@
-console.log("Hello")
+const Api_Key = "a1b190e84d29a976183eb30d8832461a"; 
+
+
+
+
+
+
+/// Main code block 
+fetch(`https://api.openweathermap.org/geo/1.0/direct?q=miami&limit=5&appid=${Api_Key}`
+)
+    .then(response => response.json())
+    .then( citySearch => {
+    let firstCity = citySearch[0];
+    console.log(firstCity.lon);
+
+   return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=${Api_Key}`)
+
+})
+
+    .then(response => response.json())
+    .then( data => {
+    console.log(data)
+})
