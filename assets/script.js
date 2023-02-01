9feb497ee15173d39b7e76b445241b37aa064461
nk-mart <113395569+nk-mart@users.noreply.github.com>
2023-02-01 02:02:46 +0000

    JS update
    Created WeatherApiCall function to nest code for weatherapi cal,
    
    Updated SearchBTN EventListener to make API call base on input field txt

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,31 @@
 const Api_Key = "a1b190e84d29a976183eb30d8832461a"; 
 
-
 let SearchBtn = document.querySelector("#search-button")
 let SearchInput = document.querySelector("#search-input")
 
+function WeatherApiCall(city) {
+//Main code block 
+fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${Api_Key}`
+)
+    .then(response => response.json())
+    .then( citySearch => {
+    let firstCity = citySearch[0];
+    
 
+   return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=${Api_Key}`)
 
-/// Main code block 
-// fetch(`https://api.openweathermap.org/geo/1.0/direct?q=miami&limit=5&appid=${Api_Key}`
-// )
-//     .then(response => response.json())
-//     .then( citySearch => {
-//     let firstCity = citySearch[0];
-//     console.log(firstCity.lon);
+})
 
-//    return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=${Api_Key}`)
-
-// })
-
-//     .then(response => response.json())
-//     .then( data => {
-//     console.log(data)
-// })
-
+    .then(response => response.json())
+    .then( data => {
+    console.log(data)
+})
+}
 
 SearchBtn.addEventListener("click",function(event){
-    event.preventDefault()
-    console.log(SearchInput.value)
+    event.preventDefault();
+    console.log(SearchInput.value);
+
+    WeatherApiCall(SearchInput.value);
 })
 
